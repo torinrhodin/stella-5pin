0b7121c5cb97a1181c74e7fef65fb0abb51c5362
broadviewstation <torin@broadviewstation.com>
2018-01-09 00:56:25 -0500

    all base slides in

--- common/code/js/slide.scanner_connectivity_detail.js
+++ common/code/js/slide.scanner_connectivity_detail.js
@@ -4,18 +4,11 @@ document.addEventListener('presentationInit', function() {
 		},
 		onEnter: function(ele) {
 			console.log('enter ' + app.slideshow.current);
-			document.addEventListener('swiperight', this._swipePrev);
-			document.addEventListener('swipeleft', this._swipeNext);
 
 		},
 		onExit: function(ele) {
 			console.log('exit');
 		},
-		_swipePrev: function() {
-			app.slideshow.content.previous();
-		},
-		_swipeNext: function() {
-			app.slideshow.content.next();
-		},
+
 	};
 });
