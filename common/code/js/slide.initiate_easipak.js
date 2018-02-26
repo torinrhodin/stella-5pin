e52be5b33385a239a0127f98802a95d402328bf0
broadviewstation <torin@broadviewstation.com>
2018-02-26 18:40:31 -0500

    copy edits

---
+++ common/code/js/slide.initiate_easipak.js
@@ -0,0 +1,43 @@
+document.addEventListener('presentationInit', function() {
+	var slide = app.slide.initiate_easipak = {
+		elements: {
+			popupLink: "#initiate_easipak-pop-link",
+			popupText: "#popup-text"
+		},
+		onEnter: function(ele) {
+			console.log('enter ' + app.slideshow.current);
+
+			// Attach Swipe Events
+			document.addEventListener('swiperight', this._swipePrev);
+			document.addEventListener('swipeleft', this._swipeNext);
+
+			// Add popup click
+			app.addEvent('click', slide._launchPopup, slide.element.popupLink);
+
+			// Add popup close click
+//			app.addEvent('click', slide._removeClass, app.popup.closeLink);
+
+		},
+		onExit: function(ele) {
+			console.log('exit');
+
+			// Remove Swipe Event
+			document.removeEventListener('swiperight', this._swipePrev);
+			document.removeEventListener('swipeleft', this._swipeNext);
+		},
+		_swipePrev: function() {
+			if( app.popup.initialized === false ) {
+				app.slideshow.previous();	
+			}
+		},
+		_swipeNext: function() {
+			if( app.popup.initialized === false ) {
+				app.slideshow.next();
+			}			
+		},
+		_launchPopup: function(event) {
+			var popupText = slide.element.popupText.innerHTML;
+  		app.popup._init(popupText);
+		}
+  };
+});
